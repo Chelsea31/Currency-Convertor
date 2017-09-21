9c260404399c5dac5992ec9874aa57b119826e63
Chelsea31 <31530304+Chelsea31@users.noreply.github.com>
2017-09-21 19:36:03 +0000

    Add files via upload

--- gui.py
+++ gui.py
@@ -4,17 +4,20 @@ class currConv:
     def __init__(self,master):
 
         #element init
-        head=Label(master,text="Currency Convertor",font=("", 30))
-        inputField=Entry(master,width=50)
-        options = ["US $","UK £","IND"]
+        str="(▀̿Ĺ̯▀̿ ̿)"
+        head=Label(master,text="Currency Convertor",font=("palatino", 30))
+        inputField=Entry(master,width=30,font=("garamond",20))
+        options = ["US $","UK £","EUR €","IND ₹","JAP ¥"]
         var1=StringVar(master)
         var1.set("From")
         list1=OptionMenu(master,var1,*options)
+        list1.config(width=10)
         var2=StringVar(master)
         var2.set("To")
         list2=OptionMenu(master,var2,*options)
-        but=Button(master,text="Convert")
-        result=Label(master,text="No Currency converted yet",font=("",20))
+        list2.config(width=10)
+        but=Button(master,text="convert",font=("garamond",15))
+        result=Label(master,text=str,font=("garamond",20))
 
         #grid declarations
         head.grid(row=0,columnspan=2,padx=15,pady=15)
@@ -25,5 +28,8 @@ class currConv:
         result.grid(row=4,columnspan=2,padx=20,pady=20)
 
 root =Tk()
+#root.columnconfigure(0,weight=1)
+#root.rowconfigure(0,weight=1)
+root.resizable(0,0)
 x = currConv(root)
 root.mainloop()
