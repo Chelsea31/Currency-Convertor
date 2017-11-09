e983c57ad220d21c0a03ef7005ce14cbaca0612c
Chelsea31 <31530304+Chelsea31@users.noreply.github.com>
2017-11-09 22:16:54 +0800

    Add files via upload

--- gui.py
+++ gui.py
@@ -2,13 +2,27 @@ from tkinter import *
 from tkinter.ttk import *
 import feedparser
 from api import convert
-
-
+import requests
 
 class currConv:
     def convertCurr(self,event):
-        convert()
-        #Use this method to call your function. use str.set("") to show the result. use self.var1.get() and self.var2.get() to access the incoming and recipient currency. Use inputField.get() to access the amount to be changed.
+        toConvert = self.inputField.get()
+
+        # fetches current price for USD
+        response = requests.get(url="https://goo.gl/PbAhsZ")
+
+        # converts the response
+        jsonResponse = response.json()
+
+        # fetches list of currency
+        priceList = jsonResponse["quotes"]
+
+        # fetches USD to INR Conversion rate
+        usdinr = priceList["USDINR"]
+
+        # Output
+        strVal.set(str(float(toConvert) * usdinr))
+
 
     def __init__(self,master):
 
@@ -21,20 +35,18 @@ class currConv:
 
         #First tab
         head=Label(tab1,text="Currency Convertor",font=("palatino", 30))
-        inputField=Entry(tab1,width=30,font=("garamond",20))
+        self.inputField=Entry(tab1,width=30,font=("garamond",20))
         options = ["US $","UK £","EUR €","IND ₹","JAP ¥"]
         self.var1=StringVar(tab1)
-        self.var1.set("From")
         list1=OptionMenu(tab1,self.var1,*options)
         list1.config(width=10)
         self.var2=StringVar(tab1)
-        self.var2.set("To")
         list2=OptionMenu(tab1,self.var2,*options)
         list2.config(width=10)
         but=Button(tab1,text="convert")
         but.bind("<Button-1>",self.convertCurr)
-        result=Label(tab1,textvariable=str,font=("garamond",20))
-        str.set("(▀̿Ĺ̯▀̿ ̿)")
+        result=Label(tab1,textvariable=strVal,font=("garamond",20))
+        strVal.set("(▀̿Ĺ̯▀̿ ̿)")
 
         #Second tab
         link = "https://www.reddit.com/r/CURRENCY/.rss"  # rss link
@@ -53,7 +65,7 @@ class currConv:
         #grid declarations
         note.grid()
         head.grid(row=0,columnspan=2,padx=15,pady=15)
-        inputField.grid(row=1,columnspan=2,sticky=N+S+E+W,padx=5,pady=5)
+        self.inputField.grid(row=1,columnspan=2,sticky=N+S+E+W,padx=5,pady=5)
         list1.grid(row=2,columnspan=1,sticky=N+S+W,padx=70,pady=10)
         list2.grid(row=2,column=1,columnspan=1,sticky=N+S+E,padx=70,pady=10)
         but.grid(row=3,columnspan=2,padx=20,pady=20)
@@ -65,7 +77,7 @@ class currConv:
 
 
 root =Tk()
-str= StringVar()
+strVal= StringVar()
 root.resizable(0,0)
 x = currConv(root)
 root.mainloop()
